140526ba9048d6d34e82149b5f919b849522bd6e
Mikael <git@evinja.se>
2016-01-25 13:31:38 +0100

    Initial commit

---
+++ RecipeSearch/src/recipesearch/RecipeSearch.java
@@ -0,0 +1,34 @@
+
+package recipesearch;
+
+import java.util.ResourceBundle;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class RecipeSearch extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        
+        ResourceBundle bundle = java.util.ResourceBundle.getBundle("recipesearch/resources/RecipeSearch");
+        
+        Parent root = FXMLLoader.load(getClass().getResource("recipe_search.fxml"), bundle);
+        
+        Scene scene = new Scene(root, 600, 400);
+        
+        stage.setTitle(bundle.getString("application.name"));
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
---
+++ RecipeSearch/src/recipesearch/RecipeSearchController.java
@@ -0,0 +1,45 @@
+
+package recipesearch;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.MenuBar;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+public class RecipeSearchController implements Initializable {
+    
+    @FXML private MenuBar menuBar;
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+    }
+    
+    @FXML 
+    protected void openAboutActionPerformed(ActionEvent event) throws IOException{
+    
+        ResourceBundle bundle = java.util.ResourceBundle.getBundle("recipesearch/resources/RecipeSearch");
+        Parent root = FXMLLoader.load(getClass().getResource("recipe_search_about.fxml"), bundle);
+        Stage aboutStage = new Stage();
+        aboutStage.setScene(new Scene(root));
+        aboutStage.setTitle(bundle.getString("about.title.text"));
+        aboutStage.initModality(Modality.APPLICATION_MODAL);
+        aboutStage.setResizable(false);
+        aboutStage.showAndWait();
+    }
+    
+    @FXML 
+    protected void closeApplicationActionPerformed(ActionEvent event) throws IOException{
+        
+        Stage addressBookStage = (Stage) menuBar.getScene().getWindow();
+        addressBookStage.hide();
+    }    
+}
